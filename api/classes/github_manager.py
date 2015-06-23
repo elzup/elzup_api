2945483085b95354a54001bdc6859195913642c1
elzzup <guild0105@gmail.com>
2015-06-23 23:34:06 +0900

    rss parse

--- api/classes/github_manager.py
+++ api/classes/github_manager.py
@@ -1,21 +1,29 @@
 import feedparser
+from bs4 import BeautifulSoup
 
 class GithubManager:
 
     RSS_URL = 'https://github.com/elzzup.atom'
 
     def __init__(self):
+        self.feed = feedparser.parse(self.RSS_URL)
         pass
 
     def get(self):
         # TODO: rss manage
-        feed = feedparser.parse(self.RSS_URL)
-        for item in feed['entries']:
+        for item in self.feed.entries:
             # NOTE: 完全な判別が出来ていなくて少々危ない
+            # NOTE: rss のHTML構造が変わらない限り有効
             if 'pushed' in item.title:
                 print(item)
-                v = item.value
-                # TODO: commit の分解
-                print(type(v))
-                print(v)
+                text = item.description
+                soup = BeautifulSoup(text)
+                li = soup.find(class_='more')
+                if not li:
+                    return
+                GithubManager.crawl_commits(li.a['href'])
 
+    @staticmethod
+    def crawl_commits(url):
+        # TODO: commit page のスクレイプ
+        print(url)
--- elzup_api/settings.py
+++ elzup_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'api',
     'feedparser',
+    'bs4',
 )
 
 MIDDLEWARE_CLASSES = (
